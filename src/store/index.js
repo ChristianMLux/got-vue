89d0852d7377c8866a30a5ca9380feb6c7c20e88
CML <christian.m.lux@gmail.com>
2021-10-04 17:09:01 +0200

    feat: adds api to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,38 @@
 import { createStore } from "vuex";
 
+let housePageCount = 9;
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    allFamilies: [],
+  },
+  mutations: {
+    setAllFamilies(state, payload) {
+      state.allFamilies = payload;
+    },
+  },
+  actions: {
+    async setAllFamilies(state) {
+      for (let i = 0; i < housePageCount; i++) {
+        let url =
+          "https://www.anapioficeandfire.com/api/houses?page=" +
+          i +
+          "&pageSize=50";
+        const apiResponse = await fetch(url, {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          method: "GET",
+        });
+        const _families = await apiResponse.json();
+        state.allFamilies.push(_families);
+      }
+    },
+  },
   modules: {},
+  getters: {
+    getAllFamilies(state) {
+      return state.allFamilies;
+    },
+  },
 });
